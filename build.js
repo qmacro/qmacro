325a42caa62c61c8e56e6394ed0b68f265a472d7
DJ Adams <dj.adams@sap.com>
2020-07-22 08:15:16 +0100

    improve dynamic content

--- build.js
+++ build.js
@@ -1,20 +1,76 @@
 const Parser = require('rss-parser')
 const parser = new Parser()
+const request = require('then-request')
 
 const Handlebars = require('handlebars')
-const source = require('./template')
-const template = Handlebars.compile(source)
+const template = Handlebars.compile(require('./template'))
 
-const URL = 'https://qmacro.org/feed.xml'
+const maxItems = 3
+
+const sources = {
+  RSS: {
+    qmacro:  'https://qmacro.org/feed.xml',
+    langram: 'https://langram.org/feed.xml',
+    youtube: 'https://www.youtube.com/feeds/videos.xml?channel_id=UCDUgrP3koL_o2iz6m55H1uA',
+  },
+  other: {
+    sap:     `https://content.services.sap.com/cs/searches/userProfile?userName=dj.adams.sap&objectTypes=blogpost&sort=published,desc&size=${maxItems}&page=0`
+  }
+}
+
+// Return a given object, but with the date property formatted nicely
+const niceDate = item => {
+  item._date = new Date(item._date).toDateString()
+  return item
+}
+
+const normalise = {
+  RSS: item => {
+    item._title = item.title
+    item._link  = item.link
+    item._date  = item.pubDate
+    return item
+  },
+  other: item => {
+    item._title = item.displayName
+    item._link  = item.url
+    item._date  = item.published
+    return item
+  }
+}
+
+// Return the latest N items from an RSS feed
+const latestRSS = async URL => {
+  const feed = await parser.parseURL(URL)
+  return feed
+    .items
+    .slice(0, maxItems)
+    .map(normalise.RSS)
+    .map(niceDate)
+}
+
+const latestContent = async URL => {
+  const res = await request('GET', URL)
+  return JSON.parse(res.getBody())
+    ._embedded.contents
+    .map(normalise.other)
+    .map(niceDate)
+}
+
 
 const main = async _ => {
   try {
-    const feed = await parser.parseURL(URL)
-    const items = feed.items.slice(0, 3).map(item => {
-      item.date = new Date(item.pubDate).toDateString()
-      return item
-    })
-    console.log(template({items}))
+    const feeds = {}
+    feeds.qmacro = await latestRSS(sources.RSS.qmacro)
+    feeds.langram = await latestRSS(sources.RSS.langram)
+    feeds.youtube = await latestRSS(sources.RSS.youtube)
+    feeds.sap = await latestContent(sources.other.sap)
+    console.log(template({
+      qmacro: feeds.qmacro,
+      langram: feeds.langram,
+      youtube: feeds.youtube,
+      sap: feeds.sap
+    }))
   } catch (error) {
     console.log(`${error}`)
     process.exit(1)
--- template.js
+++ template.js
@@ -5,9 +5,32 @@ I'm a [Developer Advocate](https://developers.sap.com/) at SAP.
 
 I've been hacking on SAP enterprise software for more than three decades, and am still enjoying it. I've [written books](https://qmacro.org/about/#writing-and-talks) for O’Reilly and SAP Press, and speak at conferences and meetups around the world, as well as online on my [YouTube channel](https://www.youtube.com/djadams-qmacro) where I run a regular live stream series called Hands-on SAP dev with qmacro.
 
+<table valign="top">
+<tr>
+<td width="50%">
 **Latest posts from [qmacro.org](https://qmacro.org)**
-{{#items}}- [{{title}}]({{link}}) ({{date}})
-{{/items}}
+{{#qmacro}}- [{{_title}}]({{_link}}) ({{_date}})
+{{/qmacro}}
+</td>
+<td>
+**Latest posts from [langram.org](https://langram.org)**
+{{#langram}}- [{{_title}}]({{_link}}) ({{_date}})
+{{/langram}}
+</td>
+</tr>
+<tr>
+<td width="50%">
+**Latest posts from the [SAP Community](https://people.sap.com/dj.adams.sap)**
+{{#sap}}- [{{_title}}]({{_link}}) ({{_date}})
+{{/sap}}
+</td>
+<td>
+**Latest live streams and videos on [my YouTube channel](https://youtube.com/djadams-qmacro)**
+{{#youtube}}- [{{_title}}]({{_link}}) ({{_date}})
+{{/youtube}}
+</td>
+</tr>
+</table>
 
 Follow me on [Twitter](https://twitter.com/qmacro), subscribe to my [YouTube channel](https://www.youtube.com/djadams-qmacro), read posts on my [personal blog](https://qmacro.org), on my [Language Ramblings blog](https://langram.org), and my [current](https://people.sap.com/dj.adams.sap#content:blogposts) & [previous](https://people.sap.com/dj.adams#content:blogposts) profiles on the [SAP Community](https://community.sap.com).
 `
