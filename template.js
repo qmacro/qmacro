dc52cdb3c9cf3d9b2a361d0c9843a195ef121ac6
DJ Adams <dj.adams@sap.com>
2020-07-22 08:29:31 +0100

    swap round table rows

--- template.js
+++ template.js
@@ -9,6 +9,20 @@ I've been hacking on SAP enterprise software for more than three decades, and am
 <tr valign="top">
 <td width="50%">
 
+**Latest posts from the [SAP Community](https://people.sap.com/dj.adams.sap)**
+{{#sap}}- [{{_title}}]({{_link}}) ({{_date}})
+{{/sap}}
+</td>
+<td>
+
+**Latest live streams and videos on [my YouTube channel](https://youtube.com/djadams-qmacro)**
+{{#youtube}}- [{{_title}}]({{_link}}) ({{_date}})
+{{/youtube}}
+</td>
+</tr>
+<tr valign="top">
+<td width="50%">
+
 **Latest posts from [qmacro.org](https://qmacro.org)**
 {{#qmacro}}- [{{_title}}]({{_link}}) ({{_date}})
 {{/qmacro}}
@@ -21,20 +35,6 @@ I've been hacking on SAP enterprise software for more than three decades, and am
 </td>
 </tr>
 
-<tr valign="top">
-<td width="50%">
-
-**Latest posts from the [SAP Community](https://people.sap.com/dj.adams.sap)**
-{{#sap}}- [{{_title}}]({{_link}}) ({{_date}})
-{{/sap}}
-</td>
-<td>
-
-**Latest live streams and videos on [my YouTube channel](https://youtube.com/djadams-qmacro)**
-{{#youtube}}- [{{_title}}]({{_link}}) ({{_date}})
-{{/youtube}}
-</td>
-</tr>
 </table>
 
 Follow me on [Twitter](https://twitter.com/qmacro), subscribe to my [YouTube channel](https://www.youtube.com/djadams-qmacro), read posts on my [personal blog](https://qmacro.org), on my [Language Ramblings blog](https://langram.org), and my [current](https://people.sap.com/dj.adams.sap#content:blogposts) & [previous](https://people.sap.com/dj.adams#content:blogposts) profiles on the [SAP Community](https://community.sap.com).
