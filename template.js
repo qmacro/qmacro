7d386b0c59d78deef0d9f120f08cca86e06b3e01
DJ Adams <qmacro@gmail.com>
2020-10-23 06:29:26 +0100

    Update template.js

--- template.js
+++ template.js
@@ -3,8 +3,7 @@ module.exports = `
 
 I'm a [Developer Advocate](https://developers.sap.com/) at SAP.
 
-I've been hacking on SAP enterprise software for more than three decades, and am still enjoying it. I've [written books](https://qmacro.org/about/#writing-and-talks) for O’Reilly and SAP Press, and speak at conferences and meetups around the world, as well as online on my [YouTube channel](https://www.youtube.com/djadams-qmacro) where I run a regular live stream series called Hands-on SAP dev with qmacro.
-
+I've been hacking on SAP enterprise software for more than three decades, and am still enjoying it. I've [written books](https://qmacro.org/about/#writing-and-talks) for O’Reilly and SAP Press, and speak at conferences and meetups around the world. Find out more [about me here](https://qmacro.org/about).
 <table>
 
 <tr valign="top">
