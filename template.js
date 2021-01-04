e867376a66a6405d171d34af603997d73aa7033b
DJ Adams <dj.adams@sap.com>
2021-01-04 13:27:08 +0000

    update template - remove table

--- template.js
+++ template.js
@@ -4,75 +4,34 @@ module.exports = `
 I'm a [Developer Advocate](https://developers.sap.com/) at SAP.
 
 I've been hacking on SAP enterprise software for more than three decades, and am still enjoying it. I've [written books](https://qmacro.org/about/#writing-and-talks) for O’Reilly and SAP Press, and speak at conferences and meetups around the world. Find out more [about me here](https://qmacro.org/about).
-<table>
-
-<tr valign="top">
-<td width="50%">
 
 **Latest posts from [Autodidactics](https://qmacro.org/autodidactics/)**
 {{#autodidactics}}- [{{_title}}]({{_link}}) ({{_date}})
 {{/autodidactics}}
 
-</td>
-<td>
-
 **Latest posts from [qmacro.org](https://qmacro.org)**
 {{#qmacro}}- [{{_title}}]({{_link}}) ({{_date}})
 {{/qmacro}}
-
-</td>
-</tr>
-
-<tr valign="top">
-<td width="50%">
 
 **Latest posts from the [SAP Community](https://people.sap.com/dj.adams.sap)**
 {{#sap}}- [{{_title}}]({{_link}}) ({{_date}})
 {{/sap}}
 
-</td>
-<td>
-
 **Latest [qmacro episodes](https://www.youtube.com/playlist?list=PLfctWmgNyOIebP3qa7jXfn68QcwS5dttb) from [SAPDevs](https://www.youtube.com/user/sapdevs) Hands-on SAP Dev show**
 {{#ytsapdevs}}- [{{_title}}]({{_link}}) ({{_date}})
 {{/ytsapdevs}}
-
-</td>
-</tr>
-
-<tr valign="top">
-<td width="50%">
 
 **Latest live streams and videos on [my YouTube channel](https://youtube.com/djadams-qmacro)**
 {{#ytqmacro}}- [{{_title}}]({{_link}}) ({{_date}})
 {{/ytqmacro}}
 
-</td>
-<td>
-
 **Latest posts from [langram.org](https://langram.org)**
 {{#langram}}- [{{_title}}]({{_link}}) ({{_date}})
 {{/langram}}
 
-</td>
-</tr>
-
-<tr valign="top">
-<td width="50%">
-
 **Latest episodes on my [Tech Aloud](https://anchor.fm/tech-aloud) podcast series**
 {{#techaloud}}- [{{_title}}]({{_link}}) ({{_date}})
 {{/techaloud}}
-
-</td>
-<td>
-
-<!--empty-->
-
-</td>
-</tr>
-
-</table>
 
 Follow me on [Twitter](https://twitter.com/qmacro), subscribe to my [YouTube channel](https://www.youtube.com/djadams-qmacro), read posts on my [personal blog](https://qmacro.org), on my [Language Ramblings blog](https://langram.org), and my [current](https://people.sap.com/dj.adams.sap#content:blogposts) & [previous](https://people.sap.com/dj.adams#content:blogposts) profiles on the [SAP Community](https://community.sap.com).
 
